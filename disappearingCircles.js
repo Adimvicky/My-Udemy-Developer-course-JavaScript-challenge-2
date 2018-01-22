f6d4d3dae5b0bc54a34c2371f41ddb992acac92c
Adim Victor <adimvicky@gmail.com>
2018-01-23 00:45:53 +0100

    Add files via upload

---
+++ disappearingCircles.js
@@ -0,0 +1,12 @@
+function dis(){
+        
+     document.getElementById("mes").style.opacity = "0";
+    }
+    function di(){
+        
+     document.getElementById("me").style.opacity = "0";
+    }
+     function d(){
+        
+     document.getElementById("m").style.opacity = "0";
+    }
